b129a2f1026f6ff6d7296199b6e912394705057d
Garvin Hicking <blog@garv.in>
2025-01-30 23:32:21 +0100

    [TASK] Rework caching, only single lookup. Interface to CLI.

--- Classes/Service/ProvideEventListService.php
+++ Classes/Service/ProvideEventListService.php
@@ -17,67 +17,119 @@ declare(strict_types=1);
 
 namespace Cru\Psr14EventList\Service;
 
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 use TYPO3\CMS\Core\Core\Environment;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Core\Package\PackageManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
-class ProvideEventListService
+final readonly class ProvideEventListService
 {
-    public function __construct() {}
+    public function __construct(
+        private PackageManager $packageManager,
+        private Typo3Version $typo3Version,
+    ) {
+    }
 
-    public function getConfiguration(): array
+    public function getConfiguration(bool $useCache = true, bool $fetchDocs = true, ?OutputInterface $cliOutput = null): array
     {
-        $packageManager = GeneralUtility::makeInstance(PackageManager::class);
         $eventClasses = [];
+        $docsUrl = 'https://docs.typo3.org/m/typo3/reference-coreapi/' .  $this->typo3Version->getBranch() . '/en-us/';
+        $docsUrlFallback = 'https://docs.typo3.org/m/typo3/reference-coreapi/main/en-us/';
 
-        foreach ($packageManager->getActivePackages() as $package) {
+        if ($fetchDocs) {
+            if ($cliOutput?->isVerbose()) {
+                $cliOutput->writeln('Using <info>' . $docsUrl . '</info> for doc lookup.');
+            }
+            $cacheFile = Environment::getVarPath() . '/cache/data/events_docs.json';
+
+            // Do a live access if either caching is disabled, or the cachefile is missing or oudated.
+            $docsCache = [];
+            if ($useCache === true
+                && file_exists($cacheFile)
+                && filemtime($cacheFile) < time()-86400
+            ) {
+                $docsCache = json_decode(file_get_contents($cacheFile), true);
+                if ($cliOutput?->isVerbose()) {
+                    $cliOutput->writeln('Retrieved <info>' . count($docsCache) . ' doc-links</info> from cache</info>');
+                }
+            } else {
+                $jsonContents = GeneralUtility::getUrl($docsUrl . 'objects.inv.json');
+
+                if ($jsonContents === false || $jsonContents === '') {
+                    // Requesting versions like '14.1', '15.0' may lead to missing link.
+                    // Use "main" as a fallback for the most recent version then.
+                    $jsonContents = GeneralUtility::getUrl($docsUrlFallback . 'objects.inv.json');
+                    if ($cliOutput?->isVerbose()) {
+                        $cliOutput->writeln('Using fallback <info>' . $docsUrlFallback . '</info> for doc lookup.');
+                    }
+                }
+
+                if ($jsonContents !== false && $jsonContents !== '') {
+                    $docuJson = json_decode($jsonContents, true);
+                    $docsCache = $docuJson['php:class'] ?? [];
+                    GeneralUtility::writeFileToTypo3tempDir($cacheFile, json_encode($docsCache));
+                    if ($cliOutput?->isVerbose()) {
+                        $cliOutput->writeln('Persisted <info>' . count($docsCache) . ' doc-links</info> to cache');
+                    }
+                } elseif ($cliOutput?->isVerbose()) {
+                    $cliOutput->writeln('<error>Could not write docs cache or invalid URL return.</error>');
+                }
+            }
+        }
+
+        $index = 0;
+        foreach ($this->packageManager->getActivePackages() as $package) {
+            $index++;
+            if ($cliOutput?->isVerbose()) {
+                $cliOutput->writeln('<info>#' . $index . '</info> Scanning <info>EXT:' . $package->getPackageKey() . '</info>');
+            }
+
             $classesPath = $package->getPackagePath() . 'Classes/';
             if (is_dir($classesPath)) {
-                $finder = new \Symfony\Component\Finder\Finder();
+                $finder = new Finder();
                 $finder->files()->in($classesPath)->name('*Event.php');
 
                 foreach ($finder as $file) {
+                    if ($cliOutput?->isVerbose()) {
+                        $cliOutput->writeln(' - <info>EXT:' . $file . '</info>');
+                    }
                     $content = file_get_contents($file->getRealPath());
 
                     if (preg_match('/namespace\s+([^;]+);/', $content, $matches)) {
                         $namespace = $matches[1];
                         $className = $namespace . '\\' . $file->getBasename('.php');
-                        $classNameDoc = str_replace('\\', '/', $className);
-                        $classNameDoc = str_replace('TYPO3/CMS/', '', $classNameDoc);
-                        $classNameDoc = str_replace('/Event/', '/', $classNameDoc);
+                        $classNameDoc = strtolower(str_replace('\\', '-', ltrim($className, '\\')));
 
                         if (class_exists($className) && !str_contains($className, 'Abstract')) {
                             $eventClasses[$className] = [
                                 'label' => $className,
                                 'package' => $package->getPackageKey(),
+                                'documentation' => '',
                             ];
 
-                            $cacheFile = Environment::getVarPath() . '/cache/data/events_docs.json';
-                            $docsCache = [];
-                            if (file_exists($cacheFile)) {
-                                $docsCache = json_decode(file_get_contents($cacheFile), true) ?? [];
+                            if (!$fetchDocs) {
+                                $eventClasses[$className]['documentation'] = '#skipped';
+                                continue;
                             }
 
-                            $cacheKey = md5($classNameDoc);
-                            if (!isset($docsCache[$cacheKey]) || $docsCache[$cacheKey]['expires'] < time()) {
-                                $docUrl = 'https://docs.typo3.org/m/typo3/reference-coreapi/13.4/en-us/ApiOverview/Events/Events/' . $classNameDoc . '.html';
-                                $headers = get_headers($docUrl, true);
-                                $exists = $headers && str_contains($headers[0], '200');
-
-                                $docsCache[$cacheKey] = [
-                                    'url' => $exists ? $docUrl : '',
-                                    'expires' => time() + 86400,
-                                ];
-
-                                GeneralUtility::writeFileToTypo3tempDir($cacheFile, json_encode($docsCache));
-                            }
-
-                            if ($docsCache[$cacheKey]['url'] !== '') {
-                                $eventClasses[$className]['documentation'] = $docsCache[$cacheKey]['url'];
+                            $eventClasses[$className]['documentation'] = '#none-found';
+                            if (isset($docsCache[$classNameDoc][2])) {
+                                $eventClasses[$className]['documentation'] = $docsUrl . $docsCache[$classNameDoc][2];
+                                if ($cliOutput?->isVeryVerbose()) {
+                                    $cliOutput->writeln('    - <info>Found <info>' . $classNameDoc . '</info> in docs php:class cache</info>');
+                                }
+                            } elseif ($cliOutput?->isVeryVerbose()) {
+                                $cliOutput->writeln('    - <info>Did not find <info>' . $classNameDoc . '</info> in docs php:class cache</info>');
                             }
                         }
                     }
                 }
+            }
+
+            if ($cliOutput?->isVerbose()) {
+                $cliOutput->writeln('');
             }
         }
 
