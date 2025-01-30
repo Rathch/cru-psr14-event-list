c54ca9b1103206e9b463b0b7da437e90213d7b24
Garvin Hicking <blog@garv.in>
2025-01-30 23:30:30 +0100

    [CLEANUP] Add TYPO3 license header

--- Classes/Backend/Controller/AdminModuleController.php
+++ Classes/Backend/Controller/AdminModuleController.php
@@ -2,8 +2,20 @@
 
 declare(strict_types=1);
 
-namespace Cru\Psr14EventList\Backend\Controller;
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
 
+namespace Cru\Psr14EventList\Backend\Controller;
 
 use TYPO3\CMS\Core\Imaging\Icon;
 use Psr\Http\Message\ResponseInterface;
